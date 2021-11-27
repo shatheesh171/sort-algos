655235d8e0820819559c1efcc46b8b3463dc5014
shatheesh17 <36799558+shatheesh17@users.noreply.github.com>
2021-11-27 22:08:01 +0530

    insertion sort

---
+++ insertion_sort.py
@@ -0,0 +1,14 @@
+def insertionSort(cl):
+    for i in range(1,len(cl)):
+        key=cl[i]
+        j=i-1
+        #Check if key is smaller than the sorted part, if it is then move elems to the right 
+        #until key can be inserted
+        while j>=0 and key<cl[j]:
+            cl[j+1]=cl[j]
+            j-=1
+        cl[j+1]=key
+    print(cl)
+
+customList=[2,1,7,6,5,3,4,9,8]
+insertionSort(customList)
